db7a17ef78912199ff5d070200069f460cd24585
Gerhard Roethlin <git@the-color-black.net>
2008-01-17 19:18:16 +0100

    Added new project: Examination Room
    - Diagram
    - Skeleton

---
+++ examinationroom/src/main.cpp
@@ -0,0 +1,22 @@
+/*******************************************************************************
+
+Examination Room is a flexible user evaluation tool
+
+Copyright (C) 2008 Gerhard Roethlin
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*******************************************************************************/
+
+
+int main (int argc, char** argv)
+{
+	return 0;
+}
