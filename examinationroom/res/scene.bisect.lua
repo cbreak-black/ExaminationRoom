69f32c07d49f5c73716be3a910e4cdebcb89b0c4
Gerhard Roethlin <git@the-color-black.net>
2008-03-20 18:03:41 +0100

    Bisect improvements
    - Voice feedback
    - Stepwise far increases (indexed)

--- examinationroom/res/scene.bisect.lua
+++ examinationroom/res/scene.bisect.lua
@@ -14,12 +14,22 @@ local permuteTable = function (t)
 end;
 
 -- The near/far limits, initial conditions
-farPoint = -16;
-farLimit = -90;
-nearPoint = 4;
+
+farPointList = {
+	0, -1, -2, -3,
+	-4, -5, -6,
+	-8, -10, -12,
+	-15, -18, -21, -24,
+	-28, -32, -36, -40,
+	-45, -50, -55, -60, -65, -70,
+	-80, -90
+}
+farPoint = 5;
+farLimit = #farPointList;
+nearPoint = 1;
 nearLimit = 9;
 
-farCorrect = 0;
+farCorrect = 2;
 farWrong = 0;
 nearCorrect = 0;
 nearWrong = 0;
@@ -28,19 +38,27 @@ farPreviousPoint = 0;
 nearPreviousCorrect = 0;
 nearPreviousPoint = 0;
 
+Dir.space = "skipped";
+
+voice = "Victoria"; -- Bruce, Vicki, Trindroids, Zarvox
+
 parseInput = function (k)
 	local d = Key[string.byte(k)];
 	if d then
 		if Dir[d] then
 			d = Dir[d];
 			if d == replies[testNum] then
+				--os.execute("say -v Victoria Correct.");
 				Scene:log("Input Correct: "..d);
 				if farTrial(testNum) then
 					farCorrect = farCorrect + 1;
 				else
 					nearCorrect = nearCorrect + 1;
 				end
+			elseif d == "skipped" then
+				os.execute("say -v "..voice.." Skipped. &");
 			else
+				os.execute("say -v "..voice.." Wrong. &");
 				Scene:log("Input Incorrect: "..d);
 				if farTrial(testNum) then
 					farWrong = farWrong + 1;
@@ -48,6 +66,8 @@ parseInput = function (k)
 					nearWrong = nearWrong + 1;
 				end
 			end;
+			Scene:log("Correct Count: Near "..nearPreviousCorrect.." Far "..farPreviousCorrect);
+
 			nextFrame();
 		elseif d == "space" then
 			--Scene:log("Input Skipped: "..d);
@@ -59,30 +79,32 @@ parseInput = function (k)
 	-- After 5 tests for far
 	if farCorrect + farWrong == 5 then
 		-- and change the position
-		if farCorrect >= 4 then
+		if farCorrect >= 5 then
 			if farPreviousPoint == farPoint then
 				farPreviousCorrect =  farPreviousCorrect + farCorrect;
 			elseif farPreviousPoint > farPoint then
 				farPreviousPoint = farPoint;
 				farPreviousCorrect = farCorrect;
 			end
-			farPoint = farPoint -1;
-			if farPoint < farLimit then
+			farPoint = farPoint +1;
+			if farPoint > farLimit then
 				farPoint = farLimit;
 				Scene:log("Far Limit reached!");
 			end
+			os.execute(string.format("say -v %s Progressing to far point %2.0f &", voice, farPointList[farPoint]));
 		else
-			farPoint = farPoint +1;
+			farPoint = farPoint -1;
+			os.execute(string.format("say -v %s Back to far point %2.0f &", voice, farPointList[farPoint]));
 		end;
 		-- reset
 		farCorrect = 0;
 		farWrong = 0;
-		Scene:log("New far point: "..farPoint);
+		Scene:log("New far point: "..farPointList[farPoint]);
 	end
 	-- After 5 tests for near
 	if nearCorrect + nearWrong == 5 then
 		-- and change the position
-		if nearCorrect >= 4 then
+		if nearCorrect >= 5 then
 			if nearPreviousPoint == nearPoint then
 				nearPreviousCorrect =  nearPreviousCorrect + nearCorrect;
 			elseif nearPreviousPoint < nearPoint then
@@ -94,17 +116,20 @@ parseInput = function (k)
 				nearPoint = nearLimit;
 				Scene:log("Near Limit reached!");
 			end
+			os.execute(string.format("say -v %s Progressing to near point %2.1f &", voice, nearPoint));
 		else
 			nearPoint = nearPoint -0.5;
+			os.execute(string.format("say -v %s Back to near point %2.1f &", voice, nearPoint));
 		end;
 		-- reset
 		nearCorrect = 0;
 		nearWrong = 0;
 		Scene:log("New near point: "..nearPoint);
 	end
-	Scene:log("Correct Count: Near "..nearPreviousCorrect.." Far "..farPreviousCorrect);
+
 	if nearPreviousCorrect > 12 and farPreviousCorrect > 12 then
 		Scene:log("Mission Completed");
+		os.execute("say -v Zarvox GAME OVER");
 		os.exit(0);
 	end
 end;
@@ -139,7 +164,7 @@ nextFrame = function ()
 	local z;
 	if farTrial(testNum) then
 		-- Test far
-		z = farPoint;
+		z = farPointList[farPoint];
 	else
 		-- Test near
 		z = nearPoint;
