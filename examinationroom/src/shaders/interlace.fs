0572ef4db39c1746f616ff3e19eef9c4d7eeb0e3
Gerhard Roethlin <git@the-color-black.net>
2009-11-12 16:53:36 +0100

    Tiny Shader improvement
    - Made shader act better for non 0 or 1 sides

--- examinationroom/src/shaders/interlace.fs
+++ examinationroom/src/shaders/interlace.fs
@@ -3,8 +3,10 @@ uniform float side;
 
 void main()
 {
-	float f = mod(floor(gl_FragCoord.y)+side,2.0); // Assigns 0 or 1 to lines depending on if they are rendered
-	if (f < 0.5)
+	// Assigns 0 or 1 to lines depending on if they are rendered
+	float f = mod(floor(gl_FragCoord.y+side),2.0);
+	if (f < 0.50)
 		discard;
+
 	gl_FragColor = gl_Color * texture2D(tex, gl_TexCoord[0].xy);
 }
